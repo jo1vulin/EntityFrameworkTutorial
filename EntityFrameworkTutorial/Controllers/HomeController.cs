92a121ad88077a437b6c95e634c9cf480c5fdabb
jo1vulin <jo1vulin@gmail.com>
2013-11-13 09:25:44 +0100

    FirstCommit

---
+++ EntityFrameworkTutorial/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameworkTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.WelcomeMessage = "Welcome to the home page";
+            return View();
+        }
+
+    }
+}
---
+++ EntityFrameworkTutorial/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EntityFrameworkTutorial.DAL;
+
+namespace EntityFrameworkTutorial.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /Student/
+
+        public SchoolContext db = new SchoolContext();
+
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+    }
+}
